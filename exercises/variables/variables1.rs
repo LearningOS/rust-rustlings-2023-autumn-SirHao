83aa509a6ae9ac1158f3f9b056dbeb9d8ab2ca6a
qcloud <ubuntu@localhost.localdomain>
2023-10-09 00:47:45 +0800

    update

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -4,10 +4,8 @@
 //
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
 fn main() {
+    let mut x: u32;
     x = 5;
     println!("x has the value {}", x);
 }
