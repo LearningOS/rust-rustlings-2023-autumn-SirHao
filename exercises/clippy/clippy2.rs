c2133c0c0aeb41e77c84d25969c99b37a6b32e5f
SirHao <2775153204@qq.com>
2023-10-16 20:03:03 +0800

    clippy2

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -7,10 +7,8 @@
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    while let Some(_x) = option {
-        if let Some(x) = option {
-            res += x
-        }
+    if let Some(x) = option {
+        res += x;
     }
     println!("{}", res);
 }
